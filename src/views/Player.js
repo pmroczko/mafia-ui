a67714ed105e9ffb581d969a8200de38b0d9aa45
Bixkog <wojciechoziebly@gmail.com>
2021-09-24 22:10:28 +0200

    Add message toast.

--- src/views/Player.js
+++ src/views/Player.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import useInterval from "../hooks/UseInterval"
@@ -9,7 +9,7 @@ function Player() {
     const queryParams = new URLSearchParams(window.location.search);
     const position = queryParams.get('position');
     const [playerState, setPlayerState] = useState(null)
-    const [messages, setMessages] = useState(null)
+    const [messages, setMessages] = useState([])
     const [publicState, setPublicState] = useState(null)
     const [targets, setTargets] = useState([])
     const [voteTarget, setVoteTarget] = useState(null)
@@ -22,7 +22,13 @@ function Player() {
         });
         MafiaService.GetPlayerMessages(position, resp => {
             if (resp.status === 200) {
+                if (JSON.stringify(resp.data) === JSON.stringify(messages)) {
+                    return
+                }
                 setMessages(resp.data);
+                for (const msg of messages) {
+                    toast(msg.text, { autoClose: 15000 })
+                }
             };
         });
         MafiaService.GetPublicState(resp => {
@@ -96,10 +102,7 @@ function Player() {
             array.push(<tr key={idx}>
                 <th scope='row'>{idx}</th>
                 {is_dead && <td><s>{name}</s></td>}
-                {!is_dead && <td>
-                    {idx === position && <ins> {name} </ins>}
-                    {idx !== position && name}
-                </td>}
+                {!is_dead && <td>{name}</td>}
                 {is_dead === false && voteTarget !== idx && <td> <Button onClick={() => addMafiaVote(idx)}> Vote </Button></td>}
                 {is_dead === false && voteTarget === idx && <td> <Button onClick={() => removeMafiaVote(idx)}> Vote </Button></td>}
                 {is_dead === false && !targets.includes(idx) && <td> <Button onClick={() => addTarget(idx)}> Target </Button></td>}
@@ -119,6 +122,8 @@ function Player() {
             {playerState != null &&
                 <div>
                     <Button onClick={showRole}> Role </Button>
+                    {publicState.time_of_day === "Day" && <h1>Day</h1>}
+                    {publicState.time_of_day !== "Day" && <h1>Night</h1>}
                 </div>
             }
             {publicState != null &&
