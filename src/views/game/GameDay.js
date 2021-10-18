4ee5cd868207249931c522a88730b0aa53d1ae5c
Bixkog <wojciechoziebly@gmail.com>
2021-10-18 23:41:37 +0200

    Add lynch confirmation modal.

--- src/views/game/GameDay.js
+++ src/views/game/GameDay.js
@@ -1,4 +1,5 @@
 import { Button, Modal } from "react-bootstrap";
+import DataController from "../../controllers/DataController";
 import MessageController from "../../controllers/MessageController";
 import ButtonClasses from "../../enums/ButtonClasses";
 import MafiaService from "../../services/MafiaService";
@@ -12,9 +13,18 @@ const GameDay = ({ playerState }) => {
     });
   }
 
+  function showLynchModal() {
+    DataController.ShowModalInfo(
+      <div>
+        <p>Are you sure?</p>
+        <Button onClick={lynchMe}> Yes, kill me! </Button>
+      </div>
+    )
+  }
+
   return (
     <div className='mafia-container'>
-      <Button onClick={lynchMe} className={ButtonClasses.BigCentered}>
+      <Button onClick={showLynchModal} className={ButtonClasses.BigCentered}>
         Lynch me!
       </Button>
     </div>
