f3611ad925b8126ba26f441e7c95559f7aa0ce97
pmroczko <58851399+pmroczko@users.noreply.github.com>
2021-10-31 11:47:07 +0100

    Added background to menu.

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -5,40 +5,45 @@ function Menu({ onClicked }) {
   var location = useLocation();
   location = location.pathname.substring(1);
   onClicked = onClicked ? onClicked : () => {};
-  return (
-    <div className='menu-container'>
-      <h1>Menu</h1>
-      <MenuLink
-        route='join'
-        text='Join Game'
-        location={location}
-        onClicked={onClicked}
-      />
-      <MenuLink
-        route='lobby'
-        text='Lobby'
-        location={location}
-        onClicked={onClicked}
-      />
-      <MenuLink
-        route='game'
-        text='Game'
-        location={location}
-        onClicked={onClicked}
-      />
-      <MenuLink
-        route='help'
-        text='Help'
-        location={location}
-        onClicked={onClicked}
-      />
-      <MenuLink
-        route='admin'
-        text='Admin'
-        location={location}
-        onClicked={onClicked}
-      />
-    </div>
+
+  const onBackgroundClick = () => {
+    onClicked();
+  }
+  return (<div>
+    <div className="menu-background" onClick={onBackgroundClick}>
+  </div>
+  <div className='menu-container'>
+    <h1>Menu</h1>
+    <MenuLink
+      route='join'
+      text='Join Game'
+      location={location}
+      onClicked={onClicked}
+    />
+    <MenuLink
+      route='lobby'
+      text='Lobby'
+      location={location}
+      onClicked={onClicked}
+    />
+    <MenuLink
+      route='game'
+      text='Game'
+      location={location}
+      onClicked={onClicked}
+    />
+    <MenuLink
+      route='help'
+      text='Help'
+      location={location}
+      onClicked={onClicked}
+    />
+    <MenuLink
+      route='admin'
+      text='Admin'
+      location={location}
+      onClicked={onClicked}
+    /></div></div>
   );
 }
 
