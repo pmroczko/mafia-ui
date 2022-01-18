0f395d8288ba9a6a3b14c6e8a4a217c58639d033
pmroczko <58851399+pmroczko@users.noreply.github.com>
2022-01-18 23:49:58 +0100

    Added Scenario List View.

---
+++ src/mocks/SimpleScenario.js
@@ -0,0 +1,19 @@
+export default {
+    "name": "Mock Scenario",
+    "day_duration": 300,
+    "night_duration": 40,
+    "raw_scenario": [
+        "Sheriff",
+        "Doctor",
+        "Citizen + BusDriver",
+        "TownRandom - Citizen",
+        "Citizen",
+        "Citizen",
+        "Citizen",
+        "BusDriver",
+        "Mafioso",
+        "Mafioso",
+        "Godfather",
+        "Survior"
+    ]
+}
